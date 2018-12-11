8dbcc0383f5a0ade67ee7c453b492df6ceb44279
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-12-11 22:06:17 +0100

    adadsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { PaymentPage } from '../pages/payment/payment';
+import { SuccessPage } from '../pages/success/success';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -19,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     PaymentPage,
+    SuccessPage,
     TabsPage
   ],
   imports: [
@@ -32,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     PaymentPage,
+    SuccessPage,
     TabsPage
   ],
   providers: [
--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SuccessPage } from '../success/success';
 
 @Component({
   selector: 'page-payment',
@@ -8,7 +9,11 @@ import { NavController } from 'ionic-angular';
 export class PaymentPage {
 
   constructor(public navCtrl: NavController) {
-
+    setTimeout(() => {
+          // this.navCtrl.popToRoot();
+          // might try this instead
+          this.navCtrl.setRoot(SuccessPage);
+      }, 2000);
   }
 
 }
